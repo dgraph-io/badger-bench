b0275ff12becda358f37deccc4448efa1b940b59
Szymon Matejczyk <smatejczyk@gmail.com>
2017-04-24 16:21:53 +0200

    Added batching writes.

--- rdb2badger/main.go
+++ rdb2badger/main.go
@@ -13,6 +13,12 @@ import (
 	"os"
 )
 
+func makeCopy(a []byte) []byte {
+	b := make([]byte, len(a))
+	copy(b, a)
+	return b
+}
+
 // Rdb2badger copies data from RocksDB store to Badger.
 func Rdb2badger(ctx context.Context, rdb *store.Store, bdb *badger.KV, records int) {
 	it := rdb.NewIterator()
@@ -22,13 +28,36 @@ func Rdb2badger(ctx context.Context, rdb *store.Store, bdb *badger.KV, records i
 
 	bar := pb.StartNew(records)
 
-	for retrieved := 0; it.Valid() && retrieved < records; it.Next() {
-		err := bdb.Put(ctx, it.Key().Data(), it.Value().Data())
-		y.Check(err)
-		y.Check(it.Err())
+	bufferSize := 1000
+	entriesBuffer := make([]*badger.Entry, bufferSize)
+	nextFree := 0
 
-		bar.Increment()
+	for retrieved := 0; it.Valid() && retrieved < records; it.Next() {
+		e := new(badger.Entry)
+		e.Key = makeCopy(it.Key().Data())
+		e.Value = makeCopy(it.Value().Data())
+
+		entriesBuffer[nextFree] = e
+		nextFree++
+
+		if nextFree == bufferSize {
+			copied := make([]*badger.Entry, bufferSize)
+			copy(copied, entriesBuffer)
+			err := bdb.Write(ctx, copied)
+			y.Check(err)
+			bar.Add(bufferSize)
+
+			nextFree = 0
+		}
+
+		y.Check(it.Err())
 		retrieved++
+	}
+
+	if nextFree > 0 {
+		err := bdb.Write(ctx, entriesBuffer[:nextFree])
+		y.Check(err)
+		bar.Add(bufferSize)
 	}
 }
 
