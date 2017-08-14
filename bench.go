cfa2bc2c927835ce038269876378ad0147b6b6a5
Deepak Jois <deepak.jois@gmail.com>
2017-08-14 08:16:36 +0530

    Force a compaction to run

--- bench.go
+++ bench.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
+
+	"github.com/dgraph-io/badger/y"
+
+	"github.com/dgraph-io/badger"
+)
+
+var (
+	flagDir = flag.String("dir", "bench-tmp", "Where data is temporarily stored.")
 )
 
 func main() {
 	fmt.Println("badger-bench")
+	flag.Parse()
+	opt := badger.DefaultOptions
+	opt.Dir = *flagDir + "/badger"
+	bdb, err := badger.NewKV(&opt)
+	y.Check(err)
+	defer bdb.Close()
+	time.Sleep(10 * time.Second)
 }
