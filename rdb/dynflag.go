18591b8dee64af4c61a067f7e9b90054c6123ee5
Lucas Wang <lucas@dgraph.io>
2019-01-29 17:58:19 -0800

    keeping the compression libraries

--- rdb/dynflag.go
+++ rdb/dynflag.go
@@ -3,7 +3,7 @@
 package rdb
 
 // #cgo CXXFLAGS: -std=c++11 -O2
-// #cgo LDFLAGS: -L/usr/local/lib -lrocksdb -lstdc++ -lm
+// #cgo LDFLAGS: -L/usr/local/lib -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy
 // #include <stdint.h>
 // #include <stdlib.h>
 // #include "rdbc.h"
